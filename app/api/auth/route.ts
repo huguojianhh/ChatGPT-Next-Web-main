05bcd06146ff889387a05e6b6c80e03ce8f706c2
Jimmy <huguojianhh@outlook.com>
2024-07-11 18:35:02 +0800

    first commit

---
+++ app/api/auth/route.ts
@@ -0,0 +1,66 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { getServerSideConfig } from "../../config/server";
+import md5 from "spark-md5";
+
+const serverConfig = getServerSideConfig();
+
+// Danger! Do not hard code any secret value here!
+// 警告！不要在这里写入任何敏感信息！
+const DANGER_CONFIG = {
+  needCode: serverConfig.needCode,
+  hideUserApiKey: serverConfig.hideUserApiKey,
+  disableGPT4: serverConfig.disableGPT4,
+  hideBalanceQuery: serverConfig.hideBalanceQuery,
+  disableFastLink: serverConfig.disableFastLink,
+  customModels: serverConfig.customModels,
+  defaultModel: serverConfig.defaultModel,
+};
+
+
+async function handle(
+  req: NextRequest
+) {
+  console.log("[auth Route] params ", req);
+
+  let body
+  if (req.method === "POST") {
+    body = await req.json();
+    console.log("[auth Route] body", body);
+
+    // 您的处理逻辑
+  }
+
+  console.log("[auth Route] params ", body);
+
+  const { accessCode, accessUserName } = body
+  console.log('aaaaaaaaa', accessUserName, accessCode)
+  const hashedCode = md5.hash(accessCode ?? "").trim();
+  const serverConfig = getServerSideConfig();
+
+  console.log([...serverConfig.accessUserName])
+  console.log("[Auth] allowed hashed userName: ", serverConfig.accessUserName);
+  console.log("[Auth] allowed hashed codes: ", [...serverConfig.codes]);
+  console.log("[Auth] got access code:", accessCode);
+  console.log("[Auth] hashed access code:", hashedCode);
+  console.log("[Time] ", new Date().toLocaleString());
+  let resp
+
+  if (serverConfig.needCode && (!serverConfig.codes.has(hashedCode) || accessUserName != serverConfig.accessUserName)) {
+    resp = {
+      error: true,
+      msg: !accessCode ? "empty access code" : "wrong access code",
+    };
+  } else {
+    resp = {
+      error: false
+    };
+  }
+  return NextResponse.json(resp)
+}
+
+
+export const GET = handle;
+export const POST = handle;
+
+export const runtime = "edge";
---
+++ app/components/auth.tsx
@@ -0,0 +1,129 @@
+import styles from "./auth.module.scss";
+import { IconButton } from "./button";
+
+import { useNavigate } from "react-router-dom";
+import { Path } from "../constant";
+import { useAccessStore } from "../store";
+import Locale from "../locales";
+
+import BotIcon from "../icons/bot.svg";
+import { useEffect, useState } from "react";
+import { getClientConfig } from "../config/client";
+import tr from "../locales/tr";
+
+export function AuthPage() {
+  const navigate = useNavigate();
+  const accessStore = useAccessStore();
+
+  const [errorText, setErrorText] = useState<string>('');
+
+  const goHome = () => navigate(Path.Home);
+  const goChat = () => navigate(Path.Chat);
+  const resetAccessCode = () => {
+    accessStore.update((access) => {
+      access.openaiApiKey = "";
+      access.accessUserName = "";
+      access.accessCode = "";
+    });
+  }; // Reset access code to empty string
+
+
+  const checkSubmit = async () => {
+    setErrorText('')
+    let result = await accessStore.checkUserAndCodeFetch(accessStore.accessUserName, accessStore.accessCode)
+    if (result) {
+      accessStore.update((access) => {
+        access.isAuth = true;
+      });
+      navigate(Path.Chat);
+    } else {
+      accessStore.update((access) => {
+        access.isAuth = false;
+      });
+      setErrorText('用户名或访问码错误！')
+    }
+  };
+
+
+
+  useEffect(() => {
+    if (getClientConfig()?.isApp) {
+      navigate(Path.Settings);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className={styles["auth-page"]}>
+      <div className={`no-dark ${styles["auth-logo"]}`}>
+        <BotIcon />
+      </div>
+
+      <div className={styles["auth-title"]}>{Locale.Auth.Title}</div>
+      <div className={styles["auth-tips"]}>
+
+        <span>
+
+          {Locale.Auth.UserNameTitle}
+
+        </span>
+
+        <input
+          className={styles["auth-input"]}
+          type="text"
+          placeholder={Locale.Auth.UserInput}
+          value={accessStore.accessUserName}
+          onChange={(e) => {
+            accessStore.update(
+              (access) => (access.accessUserName = e.currentTarget.value),
+            );
+          }}
+        />
+      </div>
+      {!accessStore.hideUserApiKey ? (
+        <>
+          <div className={styles["auth-tips"]}>
+            <span> {Locale.Auth.CodeTitle}</span>
+            <input
+              className={styles["auth-input"]}
+              type="password"
+              placeholder={Locale.Auth.Input}
+              value={accessStore.accessCode}
+              onChange={(e) => {
+                accessStore.update(
+                  (access) => (access.accessCode = e.currentTarget.value),
+                );
+              }}
+            />
+          </div>
+          {/* <input
+            className={styles["auth-input"]}
+            type="password"
+            placeholder={Locale.Settings.Access.Google.ApiKey.Placeholder}
+            value={accessStore.googleApiKey}
+            onChange={(e) => {
+              accessStore.update(
+                (access) => (access.googleApiKey = e.currentTarget.value),
+              );
+            }}
+          /> */}
+        </>
+      ) : null}
+      <div style={{ color: 'red' }} className={styles["auth-tips"]}> {errorText}</div>
+      <div className={styles["auth-actions"]}>
+        <IconButton
+          text={Locale.Auth.Confirm}
+          type="primary"
+          onClick={checkSubmit}
+        />
+        <IconButton
+          text={Locale.Auth.Reset}
+          onClick={() => {
+            resetAccessCode();
+            goHome();
+          }}
+        />
+      </div>
+    </div>
+  );
+}
